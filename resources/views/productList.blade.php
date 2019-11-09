51601bb51735b3b5611c9b3570e19aa8f6f8302f
test <>
2019-11-09 17:57:31 +0900

    修正

--- resources/views/productList.blade.php
+++ resources/views/productList.blade.php
@@ -5,7 +5,7 @@
     <div class="l-container__large p-container">
         <div class="l-page__wrapper p-page__wrapper">
             <h2 class="c-page__title">{{ __('Product List')}}</h2>
-            <convini_product_list-component :categories="{{ $categories }}" :product_list="{{ $product_list }}" ></convini_product_list-component>
+            <user_product_list-component :categories="{{ $categories }}" :product_list="{{ $product_list }}" ></user_product_list-component>
         </div>
     </div>
 </section>
