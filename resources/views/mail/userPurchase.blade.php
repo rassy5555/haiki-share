337f668ee563434a3ac486f082cc12608fa0ccef
test <>
2019-11-11 20:19:26 +0900

    メール文面修正

--- resources/views/mail/userPurchase.blade.php
+++ resources/views/mail/userPurchase.blade.php
@@ -1,6 +1,7 @@
 <p>{{ $product->product_name }}を購入しました！ <br>
     お店に取りに行ってください!!<br><br>
-    お店:{{$convini->convini_name}}　{{$convini->branch_name}}店<br>
+    お店:{{$convini->convini_name}}<br>
+    支店名:{{$convini->branch_name}}店<br>
     住所:{{$convini->address}}<br><br>
     価格:{{$product->price}}円
     賞味期限::{{$product->expiration_date}}
