fb7c8c7910bec2d82360aabe945b5a3ed701fbe3
test <>
2019-11-11 20:17:28 +0900

    メール文修正

--- resources/views/mail/userPurchase.blade.php
+++ resources/views/mail/userPurchase.blade.php
@@ -1,4 +1,4 @@
-<p>{{ $product->product_name }}を商品を購入しました！ <br>
+<p>{{ $product->product_name }}を購入しました！ <br>
     お店に取りに行ってください!!<br><br>
     お店:{{$convini->convini_name}}　{{$convini->branch_name}}店<br>
     住所:{{$convini->address}}<br><br>
